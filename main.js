5fed496576740ea4ec30202796ab2cafe9d8de21
sinkoo <69867339+sinkoo0@users.noreply.github.com>
2022-06-07 13:52:27 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+//create looping for even number 
+const evenNumber= [];
+let angka;
+for(let angka = 1; angka<=100; angka++){
+	if ((angka%2)==0){
+        evenNumber.push(angka);
+        }
+}
+console.log(evenNumber);
